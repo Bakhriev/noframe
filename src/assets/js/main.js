d7fdae4c78b6226f5c7c505ebc39c4d955771557
Bakhriev <legendofpain5112@gmail.com>
2023-09-26 23:49:13 +0500

    xd

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -26,20 +26,23 @@ const handleMouseMove = debounce(e => {
 	) {
 		if (e.pageY > 100) {
 			rect.style.filter = `blur(${Math.min(rectOffset, 60)}px)`
+			rect.style.backgroundColor = `rgb(50, 31, ${rectOffset - 10})`
 			lines.forEach(
 				line => (line.style.filter = `blur(${Math.min(rectOffset, 60)}px)`)
 			)
 		}
 	} else {
-		// const blurValue = Math.max(rectOffset - 10, 0)
 		const blurValue = Math.max((rectOffset / 10) * 2, 0)
 		rect.style.filter = `blur(${blurValue}px)`
+		rect.style.backgroundColor = `rgb(31, 77, 41)`
+
 		lines.forEach(line => (line.style.filter = `blur(${blurValue}px)`))
 	}
 
 	if (btn.offsetLeft - e.clientX < 50 && e.pageY - btnDistance < 150) {
 		if (e.pageY < 100) {
 			rect.style.filter = `blur(${Math.min(btnOffset / 2, 60)}px)`
+			rect.style.backgroundColor = `rgb(50, 31, ${rectOffset + 30})`
 			lines.forEach(
 				line => (line.style.filter = `blur(${Math.min(btnOffset, 60)}px)`)
 			)
@@ -48,3 +51,6 @@ const handleMouseMove = debounce(e => {
 }, 10)
 
 document.addEventListener('mousemove', handleMouseMove)
+
+// 	background-color: rgb(50, 31, 68);
+// 	background-color: rgb(31, 77, 41);
