6cdeda9ef8f23b5a6926dcae965bfd2fef1b3dd2
Bakhriev <legendofpain5112@gmail.com>
2023-09-26 20:41:29 +0500

    xd

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,50 +1,36 @@
 const btn = document.querySelector('.btn')
 const rect = document.querySelector('.box')
-const line1 = document.querySelector('.line-1')
-const line2 = document.querySelector('.line-2')
-const line3 = document.querySelector('.line-3')
+const lines = document.querySelectorAll('.line')
 
 const btnDistance = btn.offsetTop + btn.offsetHeight
 const rectDistance = rect.offsetTop + rect.offsetHeight
-const blurLayer = document.querySelector('.blur-layer')
 
 document.addEventListener('mousemove', e => {
-	if (e.pageY > rectDistance - 300 && e.pageX > 200) {
-		const rectOffset = ((e.pageY / rectDistance) * 100) / 3
+	const rectOffset = ((e.pageY / rectDistance) * 100) / 3
+	const btnOffset = 100 - e.pageY
 
+	if (
+		e.pageY > rectDistance - 300 &&
+		e.pageX > 200 &&
+		window.innerWidth - e.pageX > 200
+	) {
 		if (e.pageY > 100) {
-			if (rectOffset > 60) {
-				rect.style.filter = `blur(60px)`
-				line1.style.filter = `blur(60px)`
-				line2.style.filter = `blur(60px)`
-				line3.style.filter = `blur(60px)`
-			} else {
-				rect.style.filter = `blur(${rectOffset}px)`
-				line1.style.filter = `blur(${rectOffset}px)`
-				line2.style.filter = `blur(${rectOffset}px)`
-				line3.style.filter = `blur(${rectOffset}px)`
-			}
+			rect.style.filter = `blur(${Math.min(rectOffset, 60)}px)`
+			lines.forEach(
+				line => (line.style.filter = `blur(${Math.min(rectOffset, 60)}px)`)
+			)
 		}
 	} else {
-		rect.style.filter = `blur(0px)`
-		line2.style.filter = `blur(0px)`
-		line3.style.filter = `blur(0px)`
-		line1.style.filter = `blur(0px)`
+		rect.style.filter = 'blur(0px)'
+		lines.forEach(line => (line.style.filter = 'blur(0px)'))
 	}
+
 	if (btn.offsetLeft - e.clientX < 50 && e.pageY - btnDistance < 150) {
 		if (e.pageY < 100) {
-			const btnOffset = 100 - e.pageY
-			if (btnOffset > 60) {
-				rect.style.filter = `blur(60px)`
-				line1.style.filter = `blur(60px)`
-				line2.style.filter = `blur(60px)`
-				line3.style.filter = `blur(60px)`
-			} else {
-				rect.style.filter = `blur(${btnOffset}px)`
-				line1.style.filter = `blur(${btnOffset}px)`
-				line2.style.filter = `blur(${btnOffset}px)`
-				line3.style.filter = `blur(${btnOffset}px)`
-			}
+			rect.style.filter = `blur(${Math.min(btnOffset, 60)}px)`
+			lines.forEach(
+				line => (line.style.filter = `blur(${Math.min(btnOffset, 60)}px)`)
+			)
 		}
 	}
 })
